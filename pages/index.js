cf491e14076ff81973abf02878a1a7a524bb7e96
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-03-01 17:00:19 +0000

    api request ammended so that the weather data is displpayed in metric

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
   const [loading, setLoading] = useState(false);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=bristol&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=bristol&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
 
   const fetchWeather = (e) => {
     e.preventDefault()
