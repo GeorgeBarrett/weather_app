fee551c15d4bb25f401c00883bc11695e382a941
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-03-01 16:41:05 +0000

    button created to test api call

--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <button onClick={fetchWeather}>fetch data</button>
     
     </>
   )
