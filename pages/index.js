d81b70613f20f0d4c3c554b386abcf675f1813b0
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-03-01 16:20:08 +0000

    axios, useState and react icon imported. states added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import axios from 'axios';
+import { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
 
 export default function Home() {
+
+  const [city, setCity] = useState("");
+
+  const [weather, setWeather] = useState({});
+
+  const [loading, setLoading] = useState(false);
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=bristol&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
 
