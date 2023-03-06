ce9bd2343e5a75005796ca4a96de216610e65989
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-03-06 11:30:38 +0000

    spinner component complete

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -1,11 +1,13 @@
 // import Image from 'next/image';
 import React from 'react';
-// import spinner from '../public/spinner.gif';
+import Bubble from '../public/Bubble.gif';
+import Image from 'next/image';
+
 
 const Spinner = () => {
   return (
     <>
-        {/* <Image className="w-[200px] m-auto block" src={spinner} alt="loading spinner" /> */}
+        <Image src={Bubble} className="w-[200px] m-auto block" alt="loading" />
     </>
   )
 }
